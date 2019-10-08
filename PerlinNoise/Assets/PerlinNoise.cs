b2378efd51c5ef240c5f5abcf8c760fe0331bbcc
veadaodao <46975802+veadaodao@users.noreply.github.com>
2019-10-08 11:46:25 -0400

    perlin noise

---
+++ PerlinNoise/Assets/PerlinNoise.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinNoise : MonoBehaviour
+{
+    [RangeAttribute(1f, 10f)]
+    public float flatness = 1f;
+    [RangeAttribute(1f, 20f)]
+    public float frequency = 1f;
+    [RangeAttribute(1f, 10f)]
+    public int octaves = 8;
+    Texture2D image;
+    Terrain terrain;
+    Transform Transform;
+    Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        terrain = GetComponent<Terrain>();
+        image = new Texture2D(terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float[,] heightmap = terrain.terrainData.GetHeights(0, 0, terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight);
+
+        for (int i = 0; i < terrain.terrainData.heightmapHeight; ++i)
+        {
+            for (int j = 0; j < terrain.terrainData.heightmapWidth; ++j)
+            {
+
+                float x = j / (float)terrain.terrainData.heightmapWidth;
+                float y = i / (float)terrain.terrainData.heightmapHeight;
+
+
+                float current_frequency = frequency;
+                float height = 0f;
+                float amplitude = 1;
+                for (int z = 0; z < octaves; ++z)
+                {
+                    height = height + Mathf.PerlinNoise(x * frequency, y * frequency) * amplitude;
+                    amplitude /= 2;
+                    current_frequency *= 2;
+                }
+
+                heightmap[i, j] = height / flatness;
+            }
+        }
+        terrain.terrainData.SetHeights(0, 0, heightmap);
+
+    }
+}
