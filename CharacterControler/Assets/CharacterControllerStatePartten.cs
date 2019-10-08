5009088e7788e7dc1f9a616d5d8b5cf6adbbce97
veadaodao <46975802+veadaodao@users.noreply.github.com>
2019-10-08 10:24:01 -0400

    n

---
+++ CharacterControler/Assets/CharacterControllerStatePartten.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerStatePartten : MonoBehaviour
+{
+    enum PLAYER_STATE {S_WALK,S_IDLE,S_RUN,S_JUMP};
+    PLAYER_STATE state;
+    Animator anim;
+    public Rigidbody PlayerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = PLAYER_STATE.S_IDLE;
+        anim = GetComponent<Animator>();
+        PlayerRB = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (state == PLAYER_STATE.S_WALK)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                state = PLAYER_STATE.S_JUMP;
+                anim.SetTrigger("jump");
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case PLAYER_STATE.S_IDLE:
+                if(Input.GetKeyDown(KeyCode.W))
+                {
+                    state = PLAYER_STATE.S_WALK;
+                    anim.SetTrigger("walk");
+                }
+                break;
+
+            case PLAYER_STATE.S_WALK:
+                if (Input.GetKeyUp(KeyCode.W))
+                {
+                    state = PLAYER_STATE.S_IDLE;
+                    anim.SetTrigger("stop");
+                }
+                if (Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    state = PLAYER_STATE.S_RUN;
+                    anim.SetTrigger("run");
+                }
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    state = PLAYER_STATE.S_JUMP;
+                    anim.SetTrigger("jump");
+                    PlayerRB.AddForce(0, 300f, 0);
+                }
+                break;
+
+            case PLAYER_STATE.S_RUN:
+                if (Input.GetKeyUp(KeyCode.LeftShift))
+                {
+                    state = PLAYER_STATE.S_WALK;
+                    anim.SetTrigger("walk");
+                }
+                if (Input.GetKeyUp(KeyCode.W))
+                {
+                    state = PLAYER_STATE.S_IDLE;
+                    anim.SetTrigger("stop");
+                }
+                break;
+
+            case PLAYER_STATE.S_JUMP:
+                if (Input.GetKeyUp(KeyCode.Space))
+                {
+                    state = PLAYER_STATE.S_IDLE;
+                    anim.SetTrigger("stop");
+                    if (Input.GetKeyDown(KeyCode.W))
+                    {
+                        state = PLAYER_STATE.S_WALK;
+                        anim.SetTrigger("walk");
+                    }
+                }
+
+                break;
+
+        }
+    }
+}
---
+++ terrain/Assets/TerrainGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    [RangeAttribute(1f,10f)]
+    public float flatness = 1f;
+
+    Texture2D heightimage;
+    Terrain terrain;
+    // Start is called before the first frame update
+    void Start()
+    {
+        terrain = GetComponent<Terrain>();
+        heightimage = new Texture2D(terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight);
+        heightimage.LoadImage(File.ReadAllBytes("Assets/2.jpg"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float[,] heightmap = terrain.terrainData.GetHeights(0, 0, terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight);
+
+        for (int i = 0; i < terrain.terrainData.heightmapHeight; ++i)
+        {
+            for (int j = 0; j < terrain.terrainData.heightmapWidth; ++j)
+            {
+
+                float x = j / (float)terrain.terrainData.heightmapWidth;
+                float y = i / (float)terrain.terrainData.heightmapHeight;
+                float height = heightimage.GetPixel(i,j).r;
+                /*
+                float current_frequency = frequency;
+                float height = 0f;
+                float amplitude = 1;
+                for (int z = 0; z < octaves; ++z)
+                {
+                    height=height+ Mathf.PerlinNoise(i * frequency, j * frequency)*amplitude;
+                    amplitude /= 2;
+                    current_frequency *= 2;
+                }
+                */
+
+                heightmap[i, j] = height / flatness;
+            }
+        }
+        terrain.terrainData.SetHeights(0, 0, heightmap);
+    }
+}
